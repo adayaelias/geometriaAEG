8589f275140ccae80a31568bc7fe6e9c4d9a43e8
adaya-elias-gracia <adaeligra@alu.edu.gva.es>
2023-02-20 19:50:42 +0100

    nuevo método

--- geometria/src/Triangulo_AEG.java
+++ geometria/src/Triangulo_AEG.java
@@ -23,7 +23,5 @@ public class Triangulo_AEG extends FiguraGeometrica_AEG {
 		return Math.sqrt(sp * (sp- lado1) * (sp- lado2) * (sp- lado3));
 	}
 	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+
 }
