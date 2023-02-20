f071d751c1bea68c816807b2f3640336566b36e0
adaya-elias-gracia <adaeligra@alu.edu.gva.es>
2023-02-20 19:55:24 +0100

    versión comentada

--- geometria/src/Rectangulo_AEG.java
+++ geometria/src/Rectangulo_AEG.java
@@ -1,19 +1,30 @@
+/**
+ * @author Adaya Elías
+ * @version 1.2
+ * 20/02/2023
+ */
 
+//clase rectangulo que hereda de la clase figura geométrica.
 public class Rectangulo_AEG extends FiguraGeometrica_AEG {
+
+	//declaración de variables
 	private double l1;
 	private double l2;
-	
+
+	//metodo rectángulo define los lados.
 	public Rectangulo_AEG(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	//método para calcular el área
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+
+	//método para calcular el perímetro.
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
